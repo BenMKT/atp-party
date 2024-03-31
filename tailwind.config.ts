507212f854dc234085c2b5ac689e7da9a0ca0992
Benson Kiai <benson.kiai86@gmail.com>
2024-03-31 07:04:29 +0300

    Install "@tailwindcss/typography" plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,19 +2,20 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: { },
+    extend: {},
   },
   plugins: [
     require('@tailwindcss/forms'),
-    require('daisyui')
+    require('@tailwindcss/typography'),
+    require('daisyui'),
   ],
   daisyui: {
-    themes: [],
+    themes: ['corporate'],
   },
 };
 export default config;
