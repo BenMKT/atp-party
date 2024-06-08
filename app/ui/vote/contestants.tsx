46cbf10ea432f9340acdba48f21f75115a6bac88
Benson Kiai <benson.kiai86@gmail.com>
2024-06-08 23:11:23 +0300

    Reduce contestant display delay time to 10sec

--- app/ui/vote/contestants.tsx
+++ app/ui/vote/contestants.tsx
@@ -209,7 +209,7 @@ const Contestant = ({
         initial={{opacity: 0}}
         animate={{ opacity: 1}}
         transition={{
-          delay: 15,
+          delay: 10,
           duration: 10,
         }}
       >
