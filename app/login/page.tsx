69367bcd91e56291227cd831d4d3190ebf6f7e96
Benson Kiai <benson.kiai86@gmail.com>
2024-07-10 20:13:06 +0300

    Update login UI by adding opacity

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -11,13 +11,13 @@ export default function LoginPage() {
         loop
         playsInline
         className="absolute z-0 min-h-full w-auto min-w-full max-w-none"
-        src="/loginBG.mp4" 
+        src="/loginBG.mp4"
       />
       {/* Overlay to ensure content is visible on top of the video */}
       <div className="absolute z-10 min-h-dvh w-full bg-black bg-opacity-50" />
       <div className="z-20 mx-auto mt-20 flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
         <NavBar />
-        <p className="h-15 flex w-full items-end rounded-lg bg-blue-500 p-3 text-xl text-white md:h-36 md:text-3xl">
+        <p className="h-15 flex w-full items-end rounded-lg bg-blue-500 bg-opacity-50 p-3 text-xl text-white md:h-36 md:text-3xl">
           #OkoaKenya
         </p>
         <LoginForm />
