bf0f05fa203241e4768f5deafa55f82128bc6175
Benson Kiai <benson.kiai86@gmail.com>
2024-07-21 23:06:49 +0300

    YAGNI

--- prisma/migrations/20240422144354_update_bills_delete_relation/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Bills" DROP CONSTRAINT "Bills_memberId_fkey";
-
--- AddForeignKey
-ALTER TABLE "Bills" ADD CONSTRAINT "Bills_memberId_fkey" FOREIGN KEY ("memberId") REFERENCES "Members"("id") ON DELETE CASCADE ON UPDATE CASCADE;
